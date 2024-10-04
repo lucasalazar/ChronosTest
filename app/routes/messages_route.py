984fa2da900a5312d05dc7a8cdf5443f1478c148
Luca Salazar <omelhordobrasil@protonmail.com>
2024-10-04 19:20:02 -0300

    fix: route error

--- app/routes/messages_route.py
+++ app/routes/messages_route.py
@@ -6,7 +6,7 @@ import os
 
 router = APIRouter()
 
-@router.post("/receivemessage")
+@router.post("/receivemessage/")
 async def receive_message(request: Request):
     payload = await request.json()
     if payload["phone"] is not None and (payload["phone"] == "558581811515" or payload["phone"] == "554137950674"):
@@ -14,3 +14,5 @@ async def receive_message(request: Request):
         await process_message(payload["phone"], payload["text"]["message"])
     
     print("Webhook data received: ", json.dumps(payload, indent=4))
+    
+

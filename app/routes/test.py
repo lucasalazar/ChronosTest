313f9059abba3006ca70958b9b66535f68cfc96d
Luca Salazar <omelhordobrasil@protonmail.com>
2024-10-03 22:25:37 -0300

    feat: upgraded to assistant api

--- app/routes/test.py
+++ app/routes/test.py
@@ -1,8 +1,12 @@
 from fastapi import APIRouter
-from app.services.openai_service import assistant_chat
+from app.services.openai_service import create_assistant, create_thread, create_message, run_message
 
 router = APIRouter()
 
 @router.get("/")
 async def test():
-    return await assistant_chat("5585981811515", "Olá, tudo bem? você gosta de biscoito?")
+    thread = await create_thread()
+    message = await create_message("thread_t2kTjuuyniL0KNQo40QZ7ae7", "user", "Aqui pra belo horizonte")
+    run = await run_message("thread_t2kTjuuyniL0KNQo40QZ7ae7", "asst_Bz9cEgXxey0R5FhV7rYiiBqt")
+    
+    return {"thread": thread, "message": message,"run": run}
--- app/services/openai_service.py
+++ app/services/openai_service.py
@@ -1,5 +1,5 @@
 from fastapi import HTTPException
-import openai
+from openai import OpenAI
 import os
 from dotenv import load_dotenv
 import requests
@@ -8,88 +8,142 @@ from datetime import datetime
 
 load_dotenv()
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
 weather_api_key = os.getenv("WEATHER_API_KEY")
 
-user_data = {}
+client = OpenAI()
 
-async def get_external_data(location: str, date_formatted: str):
-    print(date_formatted)
+
+# Função para buscar dados da API externa (previsão do tempo)
+async def get_weather_data(location: str, date_formatted: str):
     url = f"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{location}/{date_formatted}?unitGroup=metric&key={weather_api_key}&contentType=json"
     response = requests.get(url)
 
     if response.status_code != 200:
         raise HTTPException(status_code=500, detail="Erro ao acessar API externa")
     
-    tempeature_info = response.json()["days"][0]
+    temperature_info = response.json()["days"][0]
     location = response.json()["resolvedAddress"]
     
-    return f"informações de temperatura: {tempeature_info}, temp é a temperatura média. localização solicitada: {location}"
+    return f"Informações de temperatura: {temperature_info}, temp é a temperatura média. Localização solicitada: {location}"
 
-async def assistant_chat(user_number: str, user_input: str):
+# Função principal do Assistant com suporte a threads
+async def create_assistant():
     try:
-        current_dateTime = datetime.now()
-        messages = [
-            {"role": "system", "content": f"Você é um assistente que pode chamar funções externas para buscar as informações sobre previsão do tempo. a data atual é ${current_dateTime}."},
-            {"role": "user", "content": user_input}
-        ]
-
-        functions = [
-            {
-                "name": "get_external_data",
-                "description": "Busca dados da API externa baseada no parâmetro fornecido",
-                "parameters": {
+        assistant = client.beta.assistants.create(
+            model="gpt-3.5-turbo",
+            name="Weather Assistant",
+            instructions="Você é um assistente que pode chamar funções externas para buscar informações sobre previsão do tempo. Pergunte o nome do usuário e armazene isso para usar depois no fluxo, priorize por chamar ele pelo nome quando achar necessário.",
+            tools=[
+                {
+                "type": "function",
+                "function": {
+                    "name": "get_weather_data",
+                    "description": "Função para pegar os dados de previsão do tempo",
+                    "parameters": {
                     "type": "object",
                     "properties": {
                         "location": {
-                            "type": "string",
-                            "description": "A localização de onde o usuário quer buscar a previsão do tempo"
+                        "type": "string",
+                        "description": "Localização para buscar a previsão do tempo"
                         },
                         "date_formatted": {
-                            "type": "string",
-                            "description": "A data solicitada pelo usuário formatada no modelo yyyy-M-d['T'H:m:s][.SSS][X] para buscar a previsão do tempo"
+                        "type": "string",
+                        "description": "A data solicitada pelo usuário formatada no modelo yyyy-M-d['T'H:m:s][.SSS][X] para buscar a previsão do tempo"
                         }
                     },
-                    "required": ["location", "date_formatted"]
+                    "required": ["location", "date_formatted"],
+                    "additionalProperties": False
+                    },
+                    "strict": True
                 }
-            }
-        ]
+                },
+            ]
+        )
+            
+           
+        return assistant
 
-        response = openai.chat.completions.create(
-            model="gpt-3.5-turbo",
-            messages=messages,
-            functions=functions,
-            function_call="auto",
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
+
+async def create_thread():
+    try:
+        thread = client.beta.threads.create()
+
+        return thread
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
+    
+async def create_message(thread_id: str, role: str, content: str):
+    try:
+        message = client.beta.threads.messages.create(
+            thread_id=thread_id,
+            role=role,
+            content=content
         )
 
-        if response.choices[0].finish_reason == 'function_call':
-            function_call_info = response.choices[0].message.function_call
-            function_name = function_call_info.name
-            function_args = json.loads(function_call_info.arguments)
+        return message
 
-            if function_name == "get_external_data":
-                data = await get_external_data(function_args["location"], function_args["date_formatted"])
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
 
-                interpretation_prompt = (
-                    f"A resposta da função é:\n{json.dumps(data)}\n"
-                    "Por favor, extraia as informações de temperatura e as condições meteorológicas da data solicitada, traduza para português e também fale a cidade que foi solicitada. Sempre seja gentil e utilize emojis quando achar necessário."
+async def run_message(thread_id: str, assistant_id: str):
+    try:
+        run = client.beta.threads.runs.create_and_poll(
+            thread_id=thread_id,
+            assistant_id=assistant_id
+        )
+        
+        if run.status == 'completed':
+            messages = client.beta.threads.messages.list(
+                thread_id=thread_id
+            )
+            print("\nMESSAGES 1:", messages)
+            return messages
+        else:
+            print("\nRUN STATUS 1:", run.status)
+        
+        # Define the list to store tool outputs
+        tool_outputs = []
+        
+        # Loop through each tool in the required action section
+        if run.required_action is not None:
+            for tool in run.required_action.submit_tool_outputs.tool_calls:
+                if tool.function.name == "get_weather_data":
+                    print("TOOL:", json.loads(tool.function.arguments))
+                    data = await get_weather_data(json.loads(tool.function.arguments)["location"], json.loads(tool.function.arguments)["date_formatted"])
+                    tool_outputs.append({
+                    "tool_call_id": tool.id,
+                    "output": data
+                    })
+            
+        # Submit all tool outputs at once after collecting them in a list
+        if tool_outputs:
+            try:
+                run = client.beta.threads.runs.submit_tool_outputs_and_poll(
+                thread_id=thread_id,
+                run_id=run.id,
+                tool_outputs=tool_outputs
                 )
-
-                interpretation_response = openai.chat.completions.create(
-                    model="gpt-3.5-turbo",
-                    messages=[
-                        {"role": "user", "content": interpretation_prompt}
-                    ]
+                print("Tool outputs submitted successfully.")
+            except Exception as e:
+                print("Failed to submit tool outputs:", e)
+            else:
+                print("No tool outputs to submit.")
+        
+        if tool_outputs:
+            if run.status == 'completed':
+                messages = client.beta.threads.messages.list(
+                    thread_id=thread_id
                 )
-
-                extracted_value = interpretation_response.choices[0].message.content
-
-                return {
-                    "message": "Função chamada com sucesso",
-                    "extracted_info": extracted_value
-                }
-
-        return {"message": response.choices[0].message.content}
+                print("\nMESSAGES 2:", messages)
+                return messages
+            else:
+                print("\RUN STATUS 2:", run.status)
 
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))

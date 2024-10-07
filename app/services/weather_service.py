9e995693f32c1a889f7d880ce98a1300e22955dd
Luca Salazar <omelhordobrasil@protonmail.com>
2024-10-07 15:31:02 -0300

    fix: fixed date format

--- app/services/weather_service.py
+++ app/services/weather_service.py
@@ -3,13 +3,16 @@ import json
 import os
 from dotenv import load_dotenv
 from fastapi import HTTPException
+from datetime import datetime
+
 
 load_dotenv()
 
 weather_api_key = os.getenv("WEATHER_API_KEY")
 
-async def get_weather_data(location: str, date_formatted: str):
-    print("Getting weather data", location, date_formatted)
+async def get_weather_data(location: str, date_original: str):
+    print("Getting weather data", location, date_original)
+    date_formatted = datetime.strptime(date_original, "%Y-%m-%dT%H:%M:%S.%f").strftime("%Y-%-m-%-dT%H:%M:%S.%f")[:-3]
     url = f"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{location}/{date_formatted}?unitGroup=metric&key={weather_api_key}&contentType=json"
     response = requests.get(url)
     print(response.status_code)
